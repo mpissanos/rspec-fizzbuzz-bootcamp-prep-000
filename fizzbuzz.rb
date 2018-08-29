02fc1c3a4ae5c48922cbcc1053e43cc6fa352afa
Manuel Pissanos <mpissano@gmail.com>
2018-08-29 18:06:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,14 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-  "Fizz"
+  if (int % 3 == 0 && int % 5 == 0)
+    "FizzBuzz"
   else if int % 5 == 0
    "Buzz"
-  else if (int % 3 == 0 && int % 5 == 0)
-    "FizzBuzz"
+  else if int % 3 == 0
+  "Fizz"
    end
   end  
  end
 end
+
+
+
